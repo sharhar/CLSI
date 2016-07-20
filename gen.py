b8db0ffe61b356db52fb40abd96efc837dd9142b
sharhar <wioishahar@gmail.com>
2016-07-20 10:51:24 -0700

    a new working prototype for code generation

--- gen.py
+++ gen.py
@@ -7,7 +7,6 @@ def getContents(text, name):
 	return text[start+len(name)+2:end]
 
 class Lang:
-
 	def __init__(self, src, indexFuncs):
 		self.src = src
 		self.body = ""
@@ -22,11 +21,22 @@ class Lang:
 		for f in indexFuncs:
 			name = f.split(" ")[0]
 
-			tempList = []
-			tempList.append(f)
-			tempList.append(getContents(funcsDef, name))
+			self.funcs[name] = getContents(funcsDef, name)
 
-			self.funcs[name] = tempList
+	def func(self, name, varDict):
+		result = self.funcs[name]
+
+		for var in varDict.keys():
+			result = result.replace("$" + var, varDict[var])
+
+		self.body += (result + "\n")
+
+	def write(self, dest):
+		result = self.head + self.body + self.foot
+		file = open(dest + "/" + self.dest, "w")
+		file.write(result)
+		file.close()
+
 
 
 indexFile = open("langs/index.txt", "r")
@@ -37,6 +47,12 @@ dest = getContents(index, "dest")
 srcs = getContents(index, "langs")
 funcsRaw = getContents(index, "funcs")
 
+idxFuncs = []
+
+for f in funcsRaw.split("\n"):
+	if not f == "":
+		idxFuncs.append(f)
+
 langs = []
 
 for l in srcs.split("\n"):
@@ -44,6 +60,29 @@ for l in srcs.split("\n"):
 		file = open("langs/" + l, "r")
 		rawText = file.read()
 		file.close()
-		langs.append(Lang(rawText))
+		langs.append(Lang(rawText, idxFuncs))
 
+idxFuncsDictTemplate = {}
 
+for func in idxFuncs:
+	idxFuncsDictTemplate[func.split(" ")[0]] = func.split(" ")
+
+codeFile = open("code.txt", "r")
+code = codeFile.read()
+codeFile.close()
+
+for line in code.split("\n"):
+	if not line == "":
+		parts = line.split("#")
+		name = parts[0]
+
+		varDict = {}
+
+		for i in range(1, len(parts)):
+			varDict[idxFuncsDictTemplate[name.replace(" ", "")][i]] = parts[i]
+
+		for l in langs:
+			l.func(name.replace(" ", ""), varDict)
+
+for l in langs:
+	l.write(dest)
